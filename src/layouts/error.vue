e27e570674898ad6d5b76ab8b7248a7d81ccade8
John Bogart <j.bogart91@gmail.com>
2021-07-09 09:35:50 +0100

    404 Error message

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-if="error.statusCode === 404">404: Page not found</h1>
     <h1 v-else>An error occurred</h1>
     <nuxt-link to="/">Home</nuxt-link>
   </div>
