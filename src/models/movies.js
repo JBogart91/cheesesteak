d044e907b4abd0b76c73d5146c3eb9da7aaa0114
John Bogart <j.bogart91@gmail.com>
2021-10-22 08:51:47 +0100

    Adding movies progress

---
+++ src/models/movies.js
@@ -0,0 +1,471 @@
+export default [
+  {
+    name: '12 Angry Men',
+    year: 1957,
+    director: 'Sidney Lumet',
+    genre: ['crime', 'drama'],
+    runtime: 96
+  },
+  {
+    name: '2001: A Space Odyssey',
+    year: 1968,
+    director: 'Stanley Kubrick',
+    genre: ['adventure', 'sci-fi'],
+    runtime: 149
+  },
+  {
+    name: '3 Idiots',
+    year: 2009,
+    director: 'Rajkumar Hirani',
+    genre: ['comedy', 'drama'],
+    runtime: 170
+  },
+  {
+    name: 'A Clockwork Orange',
+    year: 1971,
+    director: 'Stanley Kubrick',
+    genre: ['crime', 'drama', 'sci-fi'],
+    runtime: 136
+  },
+  {
+    name: 'Airplane!',
+    year: 1980,
+    director: ['Jim Abrahams', 'David Zucker', 'Jerry Zucker'],
+    genre: ['comedy'],
+    runtime: 88
+  },
+  {
+    name: 'Alien',
+    year: 1979,
+    director: 'Ridley Scott',
+    genre: ['horror', 'sci-fi'],
+    runtime: 117
+  },
+  {
+    name: 'Amadeus',
+    year: 1984,
+    director: 'Milos Forman',
+    genre: ['biography', 'history', 'drama'],
+    runtime: 160
+  },
+  {
+    name: 'American History X',
+    year: 1998,
+    director: 'Tony Kaye',
+    genre: ['drama'],
+    runtime: 119
+  },
+  {
+    name: 'American Psycho',
+    year: 2000,
+    director: 'Mary Harron',
+    genre: ['drama', 'crime', 'comedy'],
+    runtime: 101
+  },
+  {
+    name: 'Amélie',
+    year: 2001,
+    director: 'Jean-Pierre Jeunet',
+    genre: ['comedy', 'romance'],
+    runtime: 122
+  },
+  {
+    name: 'Apocolypse Now',
+    year: 1979,
+    director: 'Francis Ford Coppola',
+    genre: ['drama', 'mystery', 'war'],
+    runtime: 147
+  },
+  {
+    name: 'Baahubali: The Beginning',
+    year: 2015,
+    director: 'S. S. Rajamouli',
+    genre: ['action', 'drama'],
+    runtime: 159
+  },
+  {
+    name: 'Back to the Future',
+    year: 1985,
+    director: 'Robert Zemeckis',
+    genre: ['comedy', 'adventure', 'sci-fi'],
+    runtime: 116
+  },
+  {
+    name: 'Blade Runner',
+    year: 1982,
+    director: 'Ridley Scott',
+    genre: ['action', 'sci-fi', 'thriller'],
+    runtime: 117
+  },
+  {
+    name: 'Boyz n the Hood',
+    year: 1991,
+    director: 'John Singleton',
+    genre: ['crime', 'drama'],
+    runtime: 112
+  },
+  {
+    name: 'Breakfast at Tiffany\'s',
+    year: 1961,
+    director: 'Blake Edwards',
+    genre: ['comedy', 'drama', 'romance'],
+    runtime: 115
+  },
+  {
+    name: 'Brokeback Mountain',
+    year: 2004,
+    director: 'Ang Lee',
+    genre: ['romance', 'drama'],
+    runtime: 134
+  },
+  {
+    name: 'Casablanca',
+    year: 1942,
+    director: 'Michael Curitz',
+    genre: ['drama', 'romance', 'war'],
+    runtime: 102
+  },
+  {
+    name: 'Casino Royale',
+    year: 2006,
+    director: 'Martin Cambpell',
+    genre: ['action', 'adventure', 'thriller'],
+    runtime: 144
+  },
+  {
+    name: 'Citizen Kane',
+    year: 1941,
+    director: 'Orson Welles',
+    genre: ['drama', 'mystery'],
+    runtime: 119
+  },
+  {
+    name: 'City of God',
+    year: 2002,
+    director: ['Fernando Meirelles', 'Kátia Lund'],
+    genre: ['crime', 'drama'],
+    runtime: 130
+  },
+  {
+    name: 'Dirty Dancing',
+    year: 1987,
+    director: 'Emile Ardolino',
+    genre: ['drama', 'music', 'romance'],
+    runtime: 100
+  },
+  {
+    name: 'Dr Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+    year: 1964,
+    director: 'Stanley Kubrick',
+    genre: ['comedy'],
+    runtime: 95
+  },
+  {
+    name: 'Drive',
+    year: 2011,
+    director: 'Nicolas Winding Refn',
+    genre: ['crime', 'drama'],
+    runtime: 100
+  },
+  {
+    name: 'E.T. the Extra-Terrestrial',
+    year: 1982,
+    director: 'Steven Spielberg',
+    genre: ['family', 'sci-fi'],
+    runtime: 115
+  },
+  {
+    name: 'Enter the Dragon',
+    year: 1973,
+    director: 'Robert Clouse',
+    genre: ['action', 'crime', 'drama'],
+    runtime: 102
+  },
+  {
+    name: 'Fight Club',
+    year: 1999,
+    director: 'David Fincher',
+    genre: ['drama'],
+    runtime: 139
+  },
+  {
+    name: 'Forrest Gump',
+    year: 1994,
+    director: 'Robert Zemeckis',
+    genre: ['drama', 'romance'],
+    runtime: 142
+  },
+  {
+    name: 'Four Weddings and a Funeral',
+    year: 1994,
+    director: 'Mike Newell',
+    genre: ['comedy', 'drama', 'romance'],
+    runtime: 117
+  },
+  {
+    name: 'Gangs of New York',
+    year: 2002,
+    director: 'Martin Scorsese',
+    genre: ['crime', 'drama'],
+    runtime: 167
+  },
+  {
+    name: 'Ghostbusters',
+    year: 1984,
+    director: 'Ivan Reitman',
+    genre: ['action', 'comedy', 'fantasy'],
+    runtime: 105
+  },
+  {
+    name: 'Gladiator',
+    year: 2000,
+    director: 'Ridley Scott',
+    genre: ['action', 'adventure', 'drama'],
+    runtime: 155
+  },
+  {
+    name: 'Good Will Hunting',
+    year: 1997,
+    director: 'Gus Van Sant',
+    genre: ['drama', 'romance'],
+    runtime: 126
+  },
+  {
+    name: 'Groundhog Day',
+    year: 1993,
+    director: 'Harold Ramis',
+    genre: ['comedy', 'fantasy', 'romance'],
+    runtime: 101
+  },
+  {
+    name: 'Halloween',
+    year: 1978,
+    director: 'John Carpenter',
+    genre: ['horror', 'thriller'],
+    runtime: 91
+  },
+  {
+    name: 'Her',
+    year: 2013,
+    director: 'Spike Jonze',
+    genre: ['drama', 'romance', 'sci-fi'],
+    runtime: 126
+  },
+  {
+    name: 'In Brugges',
+    year: 2008,
+    director: 'Martin McDonagh',
+    genre: ['comedy', 'crime', 'drama'],
+    runtime: 107
+  },
+  {
+    name: 'Indiana Jones and the Raiders of the Lost Ark',
+    year: 1981,
+    director: 'Steven Spielberg',
+    genre: ['action', 'adventure'],
+    runtime: 115
+  },
+  {
+    name: 'Jaws',
+    year: 1975,
+    director: 'Steven Spielberg',
+    genre: ['action', 'thriller'],
+    runtime: 124
+  },
+  {
+    name: 'Jurassic Park',
+    year: 1993,
+    director: 'Steven Spielberg',
+    genre: ['action', 'adventure', 'sci-fi'],
+    runtime: 127
+  },
+  {
+    name: 'Lagaan: Once Upon a Time in India',
+    year: 2001,
+    director: 'Ashutosh Gowariker',
+    genre: ['drama', 'musical', 'sport'],
+    runtime: 224
+  },
+  {
+    name: 'Lawrence of Arabia',
+    year: 1962,
+    director: 'David Lean',
+    genre: ['adventure', 'biography', 'drama'],
+    runtime: 228
+  },
+  {
+    name: 'Little Miss Sunshin',
+    year: 2006,
+    director: ['Jonathon Dayton', 'Valerie Faris'],
+    genre: ['comedy', 'drama'],
+    runtime: 101
+  },
+  {
+    name: 'Léon: The Professional',
+    year: 1992,
+    director: 'Luc Besson',
+    genre: ['action', 'crime', 'drama'],
+    runtime: 110
+  },
+  {
+    name: 'Mary and Max',
+    year: 2009,
+    director: 'Adam Elliot',
+    genre: ['animation', 'comedy', 'drama'],
+    runtime: 92
+  },
+  {
+    name: 'Mean Girls',
+    year: 2004,
+    director: 'Mark Waters',
+    genre: ['comedy'],
+    runtime: 97
+  },
+  {
+    name: 'Momento',
+    year: 2000,
+    director: 'Christopher Nolan',
+    genre: ['mystery', 'thriller'],
+    runtime: 113
+  },
+  {
+    name: 'Monty Python\'s Life of Brian',
+    year: 1979,
+    director: 'Terry Jones',
+    genre: ['comedy'],
+    runtime: 94
+  },
+  {
+    name: 'Moonlight',
+    year: 2016,
+    director: 'Barry Jenkins',
+    genre: ['drama'],
+    runtime: 111
+  },
+  {
+    name: 'No Country for Old Men',
+    year: 2007,
+    director: ['Joel Coen', 'Ethan Coen'],
+    genre: ['crime', 'drama', 'thriller'],
+    runtime: 122
+  },
+  {
+    name: 'Office Space',
+    year: 1999,
+    director: 'Mike Judge',
+    genre: ['comedy'],
+    runtime: 89
+  },
+  {
+    name: 'Oldboy',
+    year: 2003,
+    director: 'Park Chan-Wook',
+    genre: ['action', 'drama', 'mystery'],
+    runtime: 120
+  },
+  {
+    name: 'Pyscho',
+    year: 1960,
+    director: 'Alfred Hitchcock',
+    genre: ['horror', 'mystery', 'thriller'],
+    runtime: 109
+  },
+  {
+    name: 'Pulp Fiction',
+    year: 1994,
+    director: 'Quentin Tarantino',
+    genre: ['crime', 'drama'],
+    runtime: 154
+  },
+  {
+    name: 'Resevoir Dogs',
+    year: 1992,
+    director: 'Quentin Tarantino',
+    genre: ['crime', 'drama', 'thriller'],
+    runtime: 99
+  },
+  {
+    name: 'Rocky',
+    year: 1976,
+    director: 'John G. Avildsen',
+    genre: ['drama', 'sport'],
+    runtime: 120
+  },
+  {
+    name: 'Rosemary\'s Baby',
+    year: 1968,
+    director: 'Roman Polanksi',
+    genre: ['drama', 'horror'],
+    runtime: 137
+  },
+  {
+    name: 'Saving Private Ryan',
+    year: 1998,
+    director: 'Steven Spielberg',
+    genre: ['drama', 'war'],
+    runtime: 169
+  },
+  {
+    name: 'Scarface',
+    year: 1983,
+    director: 'Brian De Palma',
+    genre: ['crime', 'drama'],
+    runtime: 170
+  },
+  {
+    name: 'Schindler\'s List',
+    year: 1993,
+    director: 'Steven Spielberg',
+    genre: ['biography', 'drama', 'history'],
+    runtime: 195
+  },
+  {
+    name: 'Se7en',
+    year: 1995,
+    director: 'David Fincher',
+    genre: ['crime', 'drama', 'mystery'],
+    runtime: 195
+  },
+  {
+    name: 'Seven Samurai',
+    year: 1954,
+    director: 'Akira Kurosawa',
+    genre: ['action', 'adventure', 'drama'],
+    runtime: 207
+  },
+  {
+    name: 'Shaun of the Dead',
+    year: 2004,
+    director: 'Edgar Wright',
+    genre: ['comedy', 'horror'],
+    runtime: 99
+  },
+  {
+    name: 'Snatch',
+    year: 2000,
+    director: 'Guy Ritchie',
+    genre: ['comedy', 'crime'],
+    runtime: 102
+  },
+  {
+    name: 'Spirited Away',
+    year: 2001,
+    director: 'Hayao Miyazaki',
+    genre: ['animation', 'adventure', 'family'],
+    runtime: 125
+  },
+  {
+    name: 'Stand by Me',
+    year: 1986,
+    director: 'Rob Reiner',
+    genre: ['adventure', 'drama'],
+    runtime: 89
+  },
+  {
+    name: '',
+    year: 19,
+    director: '',
+    genre: [''],
+    runtime: 1
+  },
+]
