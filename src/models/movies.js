9ba21a2c0a1889037455a105a8304392a5715159
John Bogart <john.bogart@bbc.co.uk>
2021-10-22 10:42:33 +0100

    Put directors into an array

--- src/models/movies.js
+++ src/models/movies.js
@@ -2,28 +2,28 @@ export default [
   {
     name: '12 Angry Men',
     year: 1957,
-    director: 'Sidney Lumet',
+    director: ['Sidney Lumet'],
     genre: ['crime', 'drama'],
     runtime: 96
   },
   {
     name: '2001: A Space Odyssey',
     year: 1968,
-    director: 'Stanley Kubrick',
+    director: ['Stanley Kubrick'],
     genre: ['adventure', 'sci-fi'],
     runtime: 149
   },
   {
     name: '3 Idiots',
     year: 2009,
-    director: 'Rajkumar Hirani',
+    director: ['Rajkumar Hirani'],
     genre: ['comedy', 'drama'],
     runtime: 170
   },
   {
     name: 'A Clockwork Orange',
     year: 1971,
-    director: 'Stanley Kubrick',
+    director: ['Stanley Kubrick'],
     genre: ['crime', 'drama', 'sci-fi'],
     runtime: 136
   },
@@ -37,105 +37,105 @@ export default [
   {
     name: 'Alien',
     year: 1979,
-    director: 'Ridley Scott',
+    director: ['Ridley Scott'],
     genre: ['horror', 'sci-fi'],
     runtime: 117
   },
   {
     name: 'Amadeus',
     year: 1984,
-    director: 'Milos Forman',
+    director: ['Milos Forman'],
     genre: ['biography', 'history', 'drama'],
     runtime: 160
   },
   {
     name: 'American History X',
     year: 1998,
-    director: 'Tony Kaye',
+    director: ['Tony Kaye'],
     genre: ['drama'],
     runtime: 119
   },
   {
     name: 'American Psycho',
     year: 2000,
-    director: 'Mary Harron',
+    director: ['Mary Harron'],
     genre: ['drama', 'crime', 'comedy'],
     runtime: 101
   },
   {
     name: 'Amélie',
     year: 2001,
-    director: 'Jean-Pierre Jeunet',
+    director: ['Jean-Pierre Jeunet'],
     genre: ['comedy', 'romance'],
     runtime: 122
   },
   {
     name: 'Apocolypse Now',
     year: 1979,
-    director: 'Francis Ford Coppola',
+    director: ['Francis Ford Coppola'],
     genre: ['drama', 'mystery', 'war'],
     runtime: 147
   },
   {
     name: 'Baahubali: The Beginning',
     year: 2015,
-    director: 'S. S. Rajamouli',
+    director: ['S. S. Rajamouli'],
     genre: ['action', 'drama'],
     runtime: 159
   },
   {
     name: 'Back to the Future',
     year: 1985,
-    director: 'Robert Zemeckis',
+    director: ['Robert Zemeckis'],
     genre: ['comedy', 'adventure', 'sci-fi'],
     runtime: 116
   },
   {
     name: 'Blade Runner',
     year: 1982,
-    director: 'Ridley Scott',
+    director: ['Ridley Scott'],
     genre: ['action', 'sci-fi', 'thriller'],
     runtime: 117
   },
   {
     name: 'Boyz n the Hood',
     year: 1991,
-    director: 'John Singleton',
+    director: ['John Singleton'],
     genre: ['crime', 'drama'],
     runtime: 112
   },
   {
     name: 'Breakfast at Tiffany\'s',
     year: 1961,
-    director: 'Blake Edwards',
+    director: ['Blake Edwards'],
     genre: ['comedy', 'drama', 'romance'],
     runtime: 115
   },
   {
     name: 'Brokeback Mountain',
     year: 2004,
-    director: 'Ang Lee',
+    director: ['Ang Lee'],
     genre: ['romance', 'drama'],
     runtime: 134
   },
   {
     name: 'Casablanca',
     year: 1942,
-    director: 'Michael Curitz',
+    director: ['Michael Curitz'],
     genre: ['drama', 'romance', 'war'],
     runtime: 102
   },
   {
     name: 'Casino Royale',
     year: 2006,
-    director: 'Martin Cambpell',
+    director: ['Martin Cambpell'],
     genre: ['action', 'adventure', 'thriller'],
     runtime: 144
   },
   {
     name: 'Citizen Kane',
     year: 1941,
-    director: 'Orson Welles',
+    director: ['Orson Welles'],
     genre: ['drama', 'mystery'],
     runtime: 119
   },
@@ -149,147 +149,147 @@ export default [
   {
     name: 'Dirty Dancing',
     year: 1987,
-    director: 'Emile Ardolino',
+    director: ['Emile Ardolino'],
     genre: ['drama', 'music', 'romance'],
     runtime: 100
   },
   {
     name: 'Dr Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
     year: 1964,
-    director: 'Stanley Kubrick',
+    director: ['Stanley Kubrick'],
     genre: ['comedy'],
     runtime: 95
   },
   {
     name: 'Drive',
     year: 2011,
-    director: 'Nicolas Winding Refn',
+    director: ['Nicolas Winding Refn'],
     genre: ['crime', 'drama'],
     runtime: 100
   },
   {
     name: 'E.T. the Extra-Terrestrial',
     year: 1982,
-    director: 'Steven Spielberg',
+    director: ['Steven Spielberg'],
     genre: ['family', 'sci-fi'],
     runtime: 115
   },
   {
     name: 'Enter the Dragon',
     year: 1973,
-    director: 'Robert Clouse',
+    director: ['Robert Clouse'],
     genre: ['action', 'crime', 'drama'],
     runtime: 102
   },
   {
     name: 'Fight Club',
     year: 1999,
-    director: 'David Fincher',
+    director: ['David Fincher'],
     genre: ['drama'],
     runtime: 139
   },
   {
     name: 'Forrest Gump',
     year: 1994,
-    director: 'Robert Zemeckis',
+    director: ['Robert Zemeckis'],
     genre: ['drama', 'romance'],
     runtime: 142
   },
   {
     name: 'Four Weddings and a Funeral',
     year: 1994,
-    director: 'Mike Newell',
+    director: ['Mike Newell'],
     genre: ['comedy', 'drama', 'romance'],
     runtime: 117
   },
   {
     name: 'Gangs of New York',
     year: 2002,
-    director: 'Martin Scorsese',
+    director: ['Martin Scorsese'],
     genre: ['crime', 'drama'],
     runtime: 167
   },
   {
     name: 'Ghostbusters',
     year: 1984,
-    director: 'Ivan Reitman',
+    director: ['Ivan Reitman'],
     genre: ['action', 'comedy', 'fantasy'],
     runtime: 105
   },
   {
     name: 'Gladiator',
     year: 2000,
-    director: 'Ridley Scott',
+    director: ['Ridley Scott'],
     genre: ['action', 'adventure', 'drama'],
     runtime: 155
   },
   {
     name: 'Good Will Hunting',
     year: 1997,
-    director: 'Gus Van Sant',
+    director: ['Gus Van Sant'],
     genre: ['drama', 'romance'],
     runtime: 126
   },
   {
     name: 'Groundhog Day',
     year: 1993,
-    director: 'Harold Ramis',
+    director: ['Harold Ramis'],
     genre: ['comedy', 'fantasy', 'romance'],
     runtime: 101
   },
   {
     name: 'Halloween',
     year: 1978,
-    director: 'John Carpenter',
+    director: ['John Carpenter'],
     genre: ['horror', 'thriller'],
     runtime: 91
   },
   {
     name: 'Her',
     year: 2013,
-    director: 'Spike Jonze',
+    director: ['Spike Jonze'],
     genre: ['drama', 'romance', 'sci-fi'],
     runtime: 126
   },
   {
     name: 'In Brugges',
     year: 2008,
-    director: 'Martin McDonagh',
+    director: ['Martin McDonagh'],
     genre: ['comedy', 'crime', 'drama'],
     runtime: 107
   },
   {
     name: 'Indiana Jones and the Raiders of the Lost Ark',
     year: 1981,
-    director: 'Steven Spielberg',
+    director: ['Steven Spielberg'],
     genre: ['action', 'adventure'],
     runtime: 115
   },
   {
     name: 'Jaws',
     year: 1975,
-    director: 'Steven Spielberg',
+    director: ['Steven Spielberg'],
     genre: ['action', 'thriller'],
     runtime: 124
   },
   {
     name: 'Jurassic Park',
     year: 1993,
-    director: 'Steven Spielberg',
+    director: ['Steven Spielberg'],
     genre: ['action', 'adventure', 'sci-fi'],
     runtime: 127
   },
   {
     name: 'Lagaan: Once Upon a Time in India',
     year: 2001,
-    director: 'Ashutosh Gowariker',
+    director: ['Ashutosh Gowariker'],
     genre: ['drama', 'musical', 'sport'],
     runtime: 224
   },
   {
     name: 'Lawrence of Arabia',
     year: 1962,
-    director: 'David Lean',
+    director: ['David Lean'],
     genre: ['adventure', 'biography', 'drama'],
     runtime: 228
   },
@@ -303,42 +303,42 @@ export default [
   {
     name: 'Léon: The Professional',
     year: 1992,
-    director: 'Luc Besson',
+    director: ['Luc Besson'],
     genre: ['action', 'crime', 'drama'],
     runtime: 110
   },
   {
     name: 'Mary and Max',
     year: 2009,
-    director: 'Adam Elliot',
+    director: ['Adam Elliot'],
     genre: ['animation', 'comedy', 'drama'],
     runtime: 92
   },
   {
     name: 'Mean Girls',
     year: 2004,
-    director: 'Mark Waters',
+    director: ['Mark Waters'],
     genre: ['comedy'],
     runtime: 97
   },
   {
     name: 'Momento',
     year: 2000,
-    director: 'Christopher Nolan',
+    director: ['Christopher Nolan'],
     genre: ['mystery', 'thriller'],
     runtime: 113
   },
   {
     name: 'Monty Python\'s Life of Brian',
     year: 1979,
-    director: 'Terry Jones',
+    director: ['Terry Jones'],
     genre: ['comedy'],
     runtime: 94
   },
   {
     name: 'Moonlight',
     year: 2016,
-    director: 'Barry Jenkins',
+    director: ['Barry Jenkins'],
     genre: ['drama'],
     runtime: 111
   },
@@ -352,133 +352,133 @@ export default [
   {
     name: 'Office Space',
     year: 1999,
-    director: 'Mike Judge',
+    director: ['Mike Judge'],
     genre: ['comedy'],
     runtime: 89
   },
   {
     name: 'Oldboy',
     year: 2003,
-    director: 'Park Chan-Wook',
+    director: ['Park Chan-Wook'],
     genre: ['action', 'drama', 'mystery'],
     runtime: 120
   },
   {
     name: 'Pyscho',
     year: 1960,
-    director: 'Alfred Hitchcock',
+    director: ['Alfred Hitchcock'],
     genre: ['horror', 'mystery', 'thriller'],
     runtime: 109
   },
   {
     name: 'Pulp Fiction',
     year: 1994,
-    director: 'Quentin Tarantino',
+    director: ['Quentin Tarantino'],
     genre: ['crime', 'drama'],
     runtime: 154
   },
   {
     name: 'Resevoir Dogs',
     year: 1992,
-    director: 'Quentin Tarantino',
+    director: ['Quentin Tarantino'],
     genre: ['crime', 'drama', 'thriller'],
     runtime: 99
   },
   {
     name: 'Rocky',
     year: 1976,
-    director: 'John G. Avildsen',
+    director: ['John G. Avildsen'],
     genre: ['drama', 'sport'],
     runtime: 120
   },
   {
     name: 'Rosemary\'s Baby',
     year: 1968,
-    director: 'Roman Polanksi',
+    director: ['Roman Polanksi'],
     genre: ['drama', 'horror'],
     runtime: 137
   },
   {
     name: 'Saving Private Ryan',
     year: 1998,
-    director: 'Steven Spielberg',
+    director: ['Steven Spielberg'],
     genre: ['drama', 'war'],
     runtime: 169
   },
   {
     name: 'Scarface',
     year: 1983,
-    director: 'Brian De Palma',
+    director: ['Brian De Palma'],
     genre: ['crime', 'drama'],
     runtime: 170
   },
   {
     name: 'Schindler\'s List',
     year: 1993,
-    director: 'Steven Spielberg',
+    director: ['Steven Spielberg'],
     genre: ['biography', 'drama', 'history'],
     runtime: 195
   },
   {
     name: 'Se7en',
     year: 1995,
-    director: 'David Fincher',
+    director: ['David Fincher'],
     genre: ['crime', 'drama', 'mystery'],
     runtime: 195
   },
   {
     name: 'Seven Samurai',
     year: 1954,
-    director: 'Akira Kurosawa',
+    director: ['Akira Kurosawa'],
     genre: ['action', 'adventure', 'drama'],
     runtime: 207
   },
   {
     name: 'Shaun of the Dead',
     year: 2004,
-    director: 'Edgar Wright',
+    director: ['Edgar Wright'],
     genre: ['comedy', 'horror'],
     runtime: 99
   },
   {
     name: 'Snatch',
     year: 2000,
-    director: 'Guy Ritchie',
+    director: ['Guy Ritchie'],
     genre: ['comedy', 'crime'],
     runtime: 102
   },
   {
     name: 'Spirited Away',
     year: 2001,
-    director: 'Hayao Miyazaki',
+    director: ['Hayao Miyazaki'],
     genre: ['animation', 'adventure', 'family'],
     runtime: 125
   },
   {
     name: 'Stand by Me',
     year: 1986,
-    director: 'Rob Reiner',
+    director: ['Rob Reiner'],
     genre: ['adventure', 'drama'],
     runtime: 89
   },
   {
     name: 'Star Wars: Episode IV - A New Hope',
     year: 1977,
-    director: 'George Lucas',
+    director: ['George Lucas'],
     genre: ['action', 'adventure', 'fantasy'],
     runtime: 121
   },
   {
     name: 'Star Wars: Episode V - The Empire Strikes Back',
     year: 1980,
-    director: 'Irvin Kershner',
+    director: ['Irvin Kershner'],
     genre: ['action', 'adventure', 'fantasy'],
     runtime: 124
   },
   {
     name: 'Star Wars: Episode VI - Return of the Jedi',
     year: 1983,
-    director: 'Richard Marquand',
+    director: ['Richard Marquand'],
     genre: ['action', 'adventure', 'fantasy'],
     runtime: 131
   },
@@ -490,72 +490,72 @@ export default [
     runtime: 117
   },
   {
-    name: 'The Dark Knight',
+    name: ['The Dark Knight'],
     year: 2008,
-    director: 'Christopher Nolan',
+    director: ['Christopher Nolan'],
     genre: ['action', 'crime', 'drama'],
     runtime: 152
   },
   {
     name: 'The Deer Hunter',
     year: 1978,
-    director: 'Michael Cimino',
+    director: ['Michael Cimino'],
     genre: ['war', 'drama'],
     runtime: 183
   },
   {
     name: 'The Departed',
     year: 2006,
-    director: 'Martin Scorsese',
+    director: ['Martin Scorsese'],
     genre: ['crime', 'drama', 'thriller'],
     runtime: 151
   },
   {
     name: 'The Evil Dead',
     year: 1981,
-    director: 'Sam Raimi',
+    director: ['Sam Raimi'],
     genre: ['horror'],
     runtime: 85
   },
   {
     name: 'The Godfather',
     year: 1972,
-    director: 'Francis Ford Coppola',
+    director: ['Francis Ford Coppola'],
     genre: ['crime', 'drama'],
     runtime: 175
   },
   {
     name: 'The Good, the Bad and the Ugly',
     year: 1966,
-    director: 'Sergio Leone',
+    director: ['Sergio Leone'],
     genre: ['western'],
     runtime: 148
   },
   {
     name: 'The Goonies',
     year: 1985,
-    director: 'Richard Donner',
+    director: ['Richard Donner'],
     genre: ['adeventure', 'comedy', 'family'],
     runtime: 114
   },
   {
     name: 'The Grand Budapest Hotel',
     year: 2014,
-    director: 'Wes Anderson',
+    director: ['Wes Anderson'],
     genre: ['adeventure', 'comedy', 'crime'],
     runtime: 99
   },
   {
     name: 'The Great Dictator',
     year: 1940,
-    director: 'Charles Chaplin',
+    director: ['Charles Chaplin'],
     genre: ['comedy', 'drama', 'war'],
     runtime: 125
   },
   {
     name: 'The Green Mile',
     year: 1999,
-    director: 'Frank Darabont',
+    director: ['Frank Darabont'],
     genre: ['crime', 'drama', 'fantasy'],
     runtime: 189
   },
@@ -576,21 +576,21 @@ export default [
   {
     name: 'The Lord of the Rings: The Fellowship of the Ring',
     year: 2001,
-    director: 'Peter Jackson',
+    director: ['Peter Jackson'],
     genre: ['action', 'adventure', 'drama'],
     runtime: 178
   },
   {
     name: 'The Lord of the Rings: The Two Towers',
     year: 2002,
-    director: 'Peter Jackson',
+    director: ['Peter Jackson'],
     genre: ['action', 'adventure', 'drama'],
     runtime: 179
   },
   {
     name: 'The Lord of the Rings: The Return of the King',
     year: 2003,
-    director: 'Peter Jackson',
+    director: ['Peter Jackson'],
     genre: ['action', 'adventure', 'drama'],
     runtime: 201
   },
@@ -604,70 +604,70 @@ export default [
   {
     name: 'The Notebook',
     year: 2004,
-    director: 'Nick Cassavetes',
+    director: ['Nick Cassavetes'],
     genre: ['drama', 'romance'],
     runtime: 123
   },
   {
     name: 'The Pianist',
     year: 2002,
-    director: 'Roman Polanski',
+    director: ['Roman Polanski'],
     genre: ['biography', 'drama', 'music'],
     runtime: 150
   },
   {
     name: 'The Prestige',
     year: 2006,
-    director: 'Christopher Nolan',
+    director: ['Christopher Nolan'],
     genre: ['drama', 'mystery', 'sci-fi'],
     runtime: 130
   },
   {
     name: 'The Rocky Horror Picture Show',
     year: 1975,
-    director: 'Jim Sharman',
+    director: ['Jim Sharman'],
     genre: ['comedy', 'horror', 'musical'],
     runtime: 100
   },
   {
     name: 'The Shawshank Redemption',
     year: 1994,
-    director: 'Frank Darabont',
+    director: ['Frank Darabont'],
     genre: ['drama'],
     runtime: 142
   },
   {
     name: 'The Shining',
     year: 1980,
-    director: 'Stanley Kubrick',
+    director: ['Stanley Kubrick'],
     genre: ['drama', 'horror'],
     runtime: 146
   },
   {
     name: 'The Silence of the Lamb',
     year: 1991,
-    director: 'Jonathan Demme',
+    director: ['Jonathan Demme'],
     genre: ['crime', 'drama', 'thriller'],
     runtime: 118
   },
   {
     name: 'The Terminator',
     year: 1984,
-    director: 'James Cameron',
+    director: ['James Cameron'],
     genre: ['action', 'sci-fi'],
     runtime: 107
   },
   {
     name: 'The Truman Show',
     year: 1998,
-    director: 'Peter Weir',
+    director: ['Peter Weir'],
     genre: ['comedy', 'drama'],
     runtime: 103
   },
   {
     name: 'The Usual Suspects',
     year: 1995,
-    director: 'Bryan Singer',
+    director: ['Bryan Singer'],
     genre: ['crime', 'mystery', 'thriller'],
     runtime: 106
   },
@@ -681,28 +681,28 @@ export default [
   {
     name: 'This Is Spinal Tap',
     year: 1984,
-    director: 'Rob Reiner',
+    director: ['Rob Reiner'],
     genre: ['comedy', 'music'],
     runtime: 82
   },
   {
     name: 'Titanic',
     year: 1997,
-    director: 'James Cameron',
+    director: ['James Cameron'],
     genre: ['drama', 'romance'],
     runtime: 194
   },
   {
     name: 'Toy Story',
     year: 1995,
-    director: 'John Lasseter',
+    director: ['John Lasseter'],
     genre: ['animation', 'adventure', 'comedy'],
     runtime: 81
   },
   {
     name: 'Trainspotting',
     year: 1996,
-    director: 'Danny Boyle',
+    director: ['Danny Boyle'],
     genre: ['drama'],
     runtime: 93
   },
@@ -716,14 +716,14 @@ export default [
   {
     name: 'Vertigo',
     year: 1958,
-    director: 'Alfred Hitchcock',
+    director: ['Alfred Hitchcock'],
     genre: ['mystery', 'romance', 'thriller'],
     runtime: 128
   },
   {
     name: 'WALL-E',
     year: 2008,
-    director: 'Andrew Stanton',
+    director: ['Andrew Stanton'],
     genre: ['animation', 'adventure', 'comedy'],
     runtime: 98
   }
